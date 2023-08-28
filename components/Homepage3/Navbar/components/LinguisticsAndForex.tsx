0bc8f3b7df5a839d9d0515afe6bae940f6d7b63c
Karan <123349027+karan1633@users.noreply.github.com>
2023-08-28 17:12:21 +0530

    changes for multi currency done on home page

---
+++ components/Homepage3/Navbar/components/LinguisticsAndForex.tsx
@@ -0,0 +1,37 @@
+import useLinguisticsAndForexHook from "../../../../hooks/GeneralHooks/NavbarHooks/LinguisticsAndForexHook";
+import useMultilangHook from "../../../../hooks/LanguageHook/Multilanguages-hook";
+
+const LinguisticsAndForex = () => {
+  const { handleCurrencyValueChange } = useLinguisticsAndForexHook();
+  const { handleLanguageChange, multiLanguagesData } = useMultilangHook();
+
+  return (
+    <>
+      <div className="ms-5">
+        <select
+          onChange={(e: any) => handleCurrencyValueChange(e.target.value)}
+          className="ternary-select product-price  price"
+        >
+          <option value="INR">₹</option>
+          <option value="USD">$</option>
+          <option value="EUR">€</option>
+        </select>
+      </div>
+
+      <div className="">
+        <select
+          onChange={(e) => handleLanguageChange(e.target.value)}
+          className="ternary-select"
+        >
+          {multiLanguagesData?.length > 0 &&
+            multiLanguagesData !== null &&
+            multiLanguagesData.map((lang: any) => {
+              return <option value={lang.lang_code}>{lang.lang_name}</option>;
+            })}
+        </select>
+      </div>
+    </>
+  );
+};
+
+export default LinguisticsAndForex;
--- components/HomepageMaster.tsx
+++ components/HomepageMaster.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useDisplayTagHooks from "../hooks/HomePageHooks/DisplayTagHooks";
 import useHomeTopCategories from "../hooks/HomePageHooks/HomeTopCategoriesHook";
 import TernaryThemeHomeTopCategories from "./Homepage3/TernaryThemeHomeTopCategories";
@@ -6,18 +6,28 @@ import DisplayTagHome3 from "./Homepage3/DisplayTagHome3";
 import OurFeaturedBrand from "./Homepage3/OurFeaturedBrand";
 import TernaryThemeTopCategoriesBanner from "./Homepage3/TernaryThemeTopCategoriesBanner";
 import TernaryThemeHomeBanner from "./Homepage3/TernaryThemeHomeBanner";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setDefaultCurrencyValue } from "../store/slices/general_slices/multi-currency-slice";
 import { setMultiLingualData } from "../store/slices/general_slices/multilang-slice";
-const HomepageMaster = ({ default_currency_value, multi_lingual_values }: any) => {
+import { display_tags } from "../store/slices/home_page_slice/home-display-tag-slice";
+const HomepageMaster = ({
+  default_currency_value,
+  multi_lingual_values,
+}: any) => {
   const { allTagsData } = useDisplayTagHooks();
   const dispatch = useDispatch();
 
   // dispatch(setDefaultCurrencyValue(default_currency_value));
-  dispatch(setMultiLingualData(multi_lingual_values));
+  // dispatch(setMultiLingualData(multi_lingual_values));
 
   const { homeTopCategories, isLoading, selectedCurrencyVal } =
     useHomeTopCategories();
+
+  const displayTagList: any = useSelector(display_tags);
+  // const { allTagsData }: any = useDisplayTagHooks();
+
+  const [displayTagDataFromReducer, setDisplayTagDataFromReducer] =
+    useState<any>([]);
 
   const renderSectionComponent = (index: number) => {
     switch (index) {
@@ -36,6 +46,14 @@ const HomepageMaster = ({ default_currency_value, multi_lingual_values }: any) =
     }
   };
 
+  useEffect(() => {
+    if (displayTagList?.tagData?.length > 0) {
+      setDisplayTagDataFromReducer([...displayTagList.tagData]);
+    } else {
+      setDisplayTagDataFromReducer([]);
+    }
+  }, [displayTagList]);
+
   // console.log("display tag in home ", allTagsData);
 
   return (
@@ -47,7 +65,7 @@ const HomepageMaster = ({ default_currency_value, multi_lingual_values }: any) =
         selectedCurrencyVal={selectedCurrencyVal}
       />
 
-      {allTagsData?.map((data: any, index: number) => (
+      {displayTagDataFromReducer?.map((data: any, index: number) => (
         <React.Fragment key={index}>
           <DisplayTagHome3 data={data} />
           {renderSectionComponent(index)}
