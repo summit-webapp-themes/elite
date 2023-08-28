7d290162bb7c84ead53a4582dab12cca08c11ec3
unknown <shadab@8848digital.com>
2023-08-28 12:22:08 +0530

    index and home page change:sk

--- components/HomepageMaster.tsx
+++ components/HomepageMaster.tsx
@@ -6,9 +6,15 @@ import DisplayTagHome3 from "./Homepage3/DisplayTagHome3";
 import OurFeaturedBrand from "./Homepage3/OurFeaturedBrand";
 import TernaryThemeTopCategoriesBanner from "./Homepage3/TernaryThemeTopCategoriesBanner";
 import TernaryThemeHomeBanner from "./Homepage3/TernaryThemeHomeBanner";
-
-const HomepageMaster = () => {
+import { useDispatch } from "react-redux";
+import { setDefaultCurrencyValue } from "../store/slices/general_slices/multi-currency-slice";
+import { setMultiLingualData } from "../store/slices/general_slices/multilang-slice";
+const HomepageMaster = ({ default_currency_value, multi_lingual_values }: any) => {
   const { allTagsData } = useDisplayTagHooks();
+  const dispatch = useDispatch();
+
+  // dispatch(setDefaultCurrencyValue(default_currency_value));
+  dispatch(setMultiLingualData(multi_lingual_values));
 
   const { homeTopCategories, isLoading, selectedCurrencyVal } =
     useHomeTopCategories();
