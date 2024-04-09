c7121256d35d81500666142ba8e3a5894b76b120
shadab khan <shadab@8848digital.com>
2024-04-09 21:27:44 +0530

    core:password visibility icon added to all the auth pages,add to cart related change in modal:sk

--- components/reset-password.tsx
+++ components/reset-password.tsx
@@ -18,11 +18,12 @@ import {
 interface FormValues {
   email: any;
   newPassword: any;
-
   confirmPassword: any;
 }
 
 const ResetPassword: any = () => {
+  const [isPasswordRevealed, setIsPasswordRevealed] = useState<boolean>(false);
+  const [isConfirmPasswordRevealed, setIsConfirmPasswordRevealed] = useState<boolean>(false)
   const dispatch = useDispatch<any>();
   const router = useRouter();
   const initialValues: FormValues = {
@@ -50,12 +51,18 @@ const ResetPassword: any = () => {
       // }, 2000);
     }
   };
-
+  const togglePasswordVisibility = (fieldName: string) => {
+    if (fieldName === 'newPassword') {
+      setIsPasswordRevealed(!isPasswordRevealed);
+    } else if (fieldName === 'confirmPassword') {
+      setIsConfirmPasswordRevealed(!isConfirmPasswordRevealed);
+    }
+  };
   return (
     <>
-      <div className="container change_pwd margin_from_nav reset-pass-wrapper " >
+      <div className="container margin_from_nav reset-pass-wrapper" style={{maxWidth:'700px' , width:'100%'}}>
         <div className="page_heading text-center">
-          <h4 className="text-uppercase change_pwdh4">Reset Your Password</h4>
+          <h4 className="text-uppercase">Reset Your Password</h4>
         </div>
 
         <Formik
@@ -64,93 +71,123 @@ const ResetPassword: any = () => {
           onSubmit={(values, action) => handleSubmit(values, action)}
         >
           {({ handleChange }) => (
-            <FormikForm className="">
-              <div className=" text-center mt-4 ms-5 mob-reset-form-wrapper">
-                <div className="container">
-                  <div className="row mb-4 pwd_height">
-                    <div className="col-md-3 d-flex align-items-center justify-content-end mob-reset-form-title">
-                      <div className="label reset-titles-mob">
-                        <label htmlFor="" className="color-black">
-                          Email ID:
-                        </label>
-                      </div>
+            <FormikForm>
+              <div className="mt-4">
+                <div className="row">
+                  <div className="col-md-3 ">
+                    <div>
+                      <label htmlFor="" className="color-black">
+                        Email ID:
+                      </label>
                     </div>
-                    <div className="col-md-9 color-black">
-                      <div className="password_block">
-                        <Field
-                          type="email"
-                          className="password_field py-4"
-                          name="email"
-                          onChange={handleChange}
-                        />
-                        <br />
-                        <div className="error_message">
-                          <ErrorMessage name="email" />
-                        </div>
+                  </div>
+                  <div className="col-md-9 color-black">
+                    <div className="">
+                      <Field
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        onChange={handleChange}
+                      />
+                      <br />
+                      <div className="error_message">
+                        <ErrorMessage name="email" />
                       </div>
                     </div>
                   </div>
+                </div>
 
-                  <div className="row mb-4 pwd_height ">
-                    <div className="col-md-3  d-flex align-items-center justify-content-end mob-reset-form-title">
-                      <div className=" label reset-titles-mob">
-                        <label htmlFor="" className="color-black">
-                          New Password:
-                        </label>
-                      </div>
+                <div className="row mb-4">
+                  <div className="col-md-3">
+                    <div>
+                      <label htmlFor="" className="color-black">
+                        New Password:
+                      </label>
                     </div>
-                    <div className="col-md-9 color-black">
-                      <div className="password_block">
+                  </div>
+                  <div className="col-md-9 color-black">
+                    <div className="">
+                      <div className="input-group">
                         <Field
-                          type="password"
-                          className="password_field py-4"
+                          type={isPasswordRevealed ? "text" : "password"}
+                          className="form-control"
                           name="newPassword"
                           onChange={handleChange}
                         />
-                        <br />
-                        <div className="error_message">
-                          <ErrorMessage name="newPassword" />
-                        </div>
+                        <span className="input-group-text px-4">
+                          {isPasswordRevealed ? (
+                            <i
+                              className="fa fa-eye visibility_icon"
+                              onClick={() => togglePasswordVisibility('newPassword')}
+
+                            />
+                          ) : (
+                            <i
+                              className="fa fa-eye-slash visibility_icon"
+                              onClick={() => togglePasswordVisibility('newPassword')}
+                            />
+                          )}
+                        </span>
+                      </div>
+                      <br />
+                      <div className="error_message">
+                        <ErrorMessage name="newPassword" />
                       </div>
                     </div>
                   </div>
+                </div>
 
-                  <div className="row mb-4 pwd_height">
-                    <div className="col-md-3  d-flex align-items-center justify-content-end  mob-reset-form-title">
-                      <div className=" label reset-titles-mob">
-                        <label htmlFor="" className="color-black">
-                          Confirm Password:
-                        </label>
-                      </div>
+                <div className="row mb-4">
+                  <div className="col-md-3">
+                    <div>
+                      <label htmlFor="" className="color-black">
+                        Confirm Password:
+                      </label>
                     </div>
-                    <div className="col-md-9 color-black">
-                      <div className="password_block">
+                  </div>
+                  <div className="col-md-9 color-black">
+                    <div className="">
+                      <div className="input-group">
                         <Field
-                          type="password"
-                          className="password_field py-4"
+                          type={isConfirmPasswordRevealed ? "text" : "password"}
+                          className="form-control"
                           name="confirmPassword"
                           onChange={handleChange}
                         />
-                        <br />
-                        <div className="error_message">
-                          <ErrorMessage name="confirmPassword" />
-                        </div>
+                        <span className="input-group-text px-4">
+                          {isConfirmPasswordRevealed ? (
+                            <i
+                              className="fa fa-eye visibility_icon"
+                              onClick={() => togglePasswordVisibility('confirmPassword')}
+
+                            />
+                          ) : (
+                            <i
+                              className="fa fa-eye-slash visibility_icon"
+                              onClick={() => togglePasswordVisibility('confirmPassword')}
+                            />
+                          )}
+                        </span>
+                      </div>
+                      <br />
+                      <div className="error_message">
+                        <ErrorMessage name="confirmPassword" />
                       </div>
                     </div>
                   </div>
-                  <div className="custom_btn mt-4 me-5 " >
-                    <Link href="/login" >
-                      <button type="button" className="btn standard_button btn_reset color-black">
-                        BACK
-                      </button>
-                    </Link>
-                    <button
-                      type="submit"
-                      className="btn standard_button text-uppercase bold ms-5 btn_reset color-black"
-                    >
-                      RESET
+                </div>
+                <div className="custom_btn text-center mt-4 me-5 " >
+                  <Link href="/login" >
+                    <button type="button" className="btn standard_button btn_reset color-black">
+                      BACK
                     </button>
-                  </div>
+                  </Link>
+                  <button
+                    type="submit"
+                    className="btn standard_button text-uppercase bold ms-5 btn_reset color-black"
+                  >
+                    RESET
+                  </button>
                 </div>
               </div>
             </FormikForm>
